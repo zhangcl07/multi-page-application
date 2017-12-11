c316e2a39a8395684bf5afd7cfbf0b29ed56eece
zhangchuanliang <zhangchuanliang@jd.com>
2017-12-12 00:45:43 +0800

    位置移动

--- dev-client.js
+++
@@ -1,8 +0,0 @@
-require('eventsource-polyfill')
-var hotClient = require('webpack-hot-middleware/client?path=/__webpack_hmr&reload=true&quiet=true&noInfo=true')
-
-hotClient.subscribe(function (event) {
-  if (event.action === 'reload') {
-    window.location.reload()
-  }
-})
